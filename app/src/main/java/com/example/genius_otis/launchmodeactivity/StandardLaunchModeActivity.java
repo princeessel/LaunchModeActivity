25d1fa1f2aa5a6869b8ee7a175820ab8df40b145
PRINCE O ESSEL <Genius-Otis@PRINCEs-MacBook-Pro.local>
2018-10-05 04:20:54 -0400

    Initial commit

---
+++ app/src/main/java/com/example/genius_otis/launchmodeactivity/StandardLaunchModeActivity.java
@@ -0,0 +1,31 @@
+package com.example.genius_otis.launchmodeactivity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class StandardLaunchModeActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_standard_launch_mode);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int viewId = v.getId();
+        switch (viewId) {
+            case R.id.standardMode:
+                startActivity(new Intent(this, StandardLaunchModeActivity.class));
+                break;
+
+            case R.id.singleTop:
+                startActivity(new Intent(this, SingleTopModeActivity.class));
+                break;
+
+        }
+    }
+
+}
